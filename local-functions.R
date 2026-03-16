35112a08c42db3b35e7c821904457690b6425371
Dylan Beaudette <dylanbeaudette@users.noreply.github.com>
2026-03-16 11:30:51 -0700

    more ideas

--- local-functions.R
+++ local-functions.R
@@ -1,7 +1,8 @@
 
-
+#' @param f file, output from `meshtastic --nodes`
 parseNodeData <- function(f) {
   
+  # read / parse the pretty-printed version of the output
   x <- readLines(f)[-1]
   
   x <- x[grep('════', x, fixed = TRUE, invert = TRUE)]
@@ -10,11 +11,14 @@ parseNodeData <- function(f) {
   
   x <- stri_split_fixed(x, pattern = '│', simplify = TRUE)
   
+  # extract columns of data
   x <- x[, 2:20]
   
+  # column header
   h <- x[1, ]
   h <- trimws(h)
   
+  # rows of data
   x <- x[-1, ]
   
   x <- as.data.frame(x)
@@ -35,5 +39,38 @@ parseNodeData <- function(f) {
     as.numeric(gsub('°', '', x$Longitude))  
   )
   
+  # encode date/time
+  x$LastHeard <- as.POSIXct(x$LastHeard)
+  
+  # subset columns
+  x <- x[, c('User', 'ID', 'AKA', 'Role', 'Latitude', 'Longitude', 'LastHeard')]
+  
   return(x)
 }
+
+
+flattenNodeData <- function(db) {
+  
+  .s <- split(db, db$ID)
+  
+  # keep the last heard record
+  .res <- lapply(.s, \(i) {
+    
+    # multiple entries
+    if(nrow(i) > 1) {
+      i <- i[order(i$LastHeard, decreasing = TRUE), ]
+      .n <- i[1, ]
+    } else {
+      # single entry, LastHeard may be NA
+      .n <- i[1, ]
+    }
+    
+    return(.n)
+  })
+  
+  .res <- do.call('rbind', .res)
+  
+  return(.res)
+}
+
+
--- mm-data-eval.R
+++ mm-data-eval.R
@@ -1,5 +1,14 @@
+## TODO:
+
+# * split between stationary / mobile nodes
+# * think about the use / interpretation of edge weights
+# * reset edge weights for nodes used to collect traceroute information
+
+
+
 library(igraph)
 library(stringi)
+library(digest)
 
 source('local-functions.R')
 
@@ -17,12 +26,13 @@ f <- list.files('node-db-files/', full.names = TRUE)
 db <- lapply(f, parseNodeData)
 db <- do.call('rbind', db)
 
-db <- db[, c('User', 'ID', 'AKA', 'Role', 'Latitude', 'Longitude')]
-db <- unique(db)
+# there should be no duplicates
+db <- flattenNodeData(db)
 nrow(db)
 
+
 # known local nodes ----
-nl <- read.csv('local-nodes.csv')
+# nl <- read.csv('local-nodes.csv')
 
 
 # review contents ----
@@ -59,7 +69,7 @@ x[idx, ]
 
 
 
- 
+
 # keep successful routes, ignore MQTT ----
 e <- grepl('traceroute', x$Type, ignore.case = TRUE) & x$Hops != '' & x$SNR != 'MQTT'
 idx <- which(e)
@@ -67,6 +77,9 @@ z <- x[idx, ]
 
 # there may be multiple versions of the same traceroute 
 sort(table(z$Data))
+
+# there should be no NA in z$Data
+stopifnot(! any(is.na(z$Data)))
 
 
 # replace user ID with short name using node DB
@@ -86,6 +99,9 @@ n <- nm[grep('^[!]', nm)]
 idx <- na.omit(match(n, db$ID))
 lut <- db[idx, c('ID', 'AKA')]
 
+# remove records from LUT missing short nams
+lut <- na.omit(lut)
+
 # replace known IDs with short name
 for(i in 1:nrow(lut)) {
   z$Data <- gsub(pattern = lut$ID[i], replacement = lut$AKA[i], x = z$Data, fixed = TRUE)
@@ -93,15 +109,25 @@ for(i in 1:nrow(lut)) {
 
 # grep('!a9e00e2f', z$Data, fixed = TRUE)
 
-lattice::dotplot(sort(table(z$Data)))
+# lattice::dotplot(sort(table(z$Data)))
+
+sort(table(z$Data))
+
+# there should be no NA in z$Data
+any(is.na(z$Data))
 
 
+# there may be duplicate routes
 # routes -> matrix
-r <- stri_split_fixed(unique(z$Data), pattern = ' -> ', simplify = FALSE)
+r <- stri_split_fixed(
+  z$Data, 
+  pattern = ' -> ', 
+  simplify = FALSE
+)
 
 toEdgeList <- function(i) {
   # reduce to graph edges via sliding window
-  .el <- sapply(1:(length(i) - 1), \(j) cbind(i[j], i[j+1]))
+  .el <- sapply(1:(length(i) - 1), \(j) cbind(i[j], i[j+1]), simplify = TRUE)
   t(.el)
 }
 
@@ -114,50 +140,140 @@ e <- do.call('rbind', e)
 idx <- which(e[, 1] != 'all' & e[, 2] != 'all')
 e <- e[idx, ]
 
+# duplicate routes should be encoded as additional edge weight
+# create edge hash for ID
+e <- as.data.frame(e)
+e$hash <- NA
+for(i in 1:nrow(e)) {
+  e$hash[i] <- digest(unlist(e[i, 1:2]))
+}
+
+# aggregate edge counts into edge weights
+tab <- table(e$hash)
+a <- lapply(names(tab), \(i) {
+  # keep the first edge definition for each hash
+  .idx <- match(i, e$hash)[1]
+  e[.idx, ]
+})
+
+a <- do.call('rbind', a)
+a$wt <- tab
+
 
 # create and style graph ----
 # treat trace routes as bi-directional links
-g <- graph_from_edgelist(e, directed = FALSE)
+g <- graph_from_edgelist(
+  as.matrix(a[, 1:2]), 
+  directed = FALSE
+)
+
+# TODO: not clear what we would actually do with this weight
+#       0xFC -> MT is the most common link because this is where the 
+#       data are coming from
+E(g)$weight <- log(a$wt) + 1
+
 
 # remove loops
 g <- simplify(g)
 
 # size ~ connectivity
-V(g)$size <- sqrt(degree(g)) * 7
+V(g)$size <- pmin(pmax(sqrt(degree(g)) * 5, 7), 15)
 
-# highlight local network
-V(g)$label.font <- 1
-V(g)$label.font[which(V(g)$name %in% nl$node)] <- 2
+# highlight routers
+.routers <- db$AKA[grep('router', db$Role, ignore.case = TRUE)]
+idx <- which(V(g)$name %in% .routers)
+V(g)$shape <- 'circle'
+V(g)$shape[idx] <- 'square'
+
+V(g)$frame.width <- 1
+V(g)$frame.width[idx] <- 1.5
+
+
+# adjust label size when short name is missing
+V(g)$label.cex <- 0.66
+idx <- which(nchar(V(g)$name) > 4)
+V(g)$label.cex[idx] <- 0.5
+
 
 par(mar = c(0, 0, 0, 0))
 
 # set.seed(1010101)
-plot(g, vertex.label.family = 'sans', vertex.color = 'white', vertex.label.color = 'black', vertex.label.cex = 0.66, edge.color = 'royalblue', layout = layout_with_lgl)
+plot(g, vertex.label.family = 'sans', vertex.color = 'white', vertex.label.color = 'black', edge.color = 'royalblue', layout = layout_with_lgl)
 title('Sierra and Surrounding Meshtastic Network', line = -1.5, sub = 'excluding MQTT')
 
 
-plot(g, vertex.label.family = 'sans', vertex.color = 'white', vertex.label.color = 'black', vertex.label.font = 2, vertex.label.cex = 0.66, edge.color = 'royalblue', layout = layout_as_tree)
+plot(g, vertex.label.family = 'sans', vertex.color = 'white', vertex.label.color = 'black', vertex.label.font = 2, edge.color = 'royalblue', layout = layout_as_tree)
+
+plot(g, vertex.label.family = 'sans', vertex.color = 'white', vertex.label.color = 'black', vertex.label.font = 2, edge.color = 'royalblue', layout = layout_as_star)
 
 
 
 g.sub <- subgraph(g, which(degree(g) > 2))
-plot(g.sub, vertex.label.family = 'sans', vertex.color = 'white', vertex.label.color = 'black', vertex.label.cex = 0.66, edge.color = 'royalblue', layout = layout_with_lgl)
+plot(g.sub, vertex.label.family = 'sans', vertex.color = 'white', vertex.label.color = 'black', edge.color = 'royalblue', layout = layout_with_lgl)
+
+plot(g.sub, vertex.label.family = 'sans', vertex.color = 'white', vertex.label.color = 'black', edge.color = 'royalblue', layout = layout_as_star)
 
 
-ragg::agg_png(filename = 'figures/sierra-ms-log-graph-01.png', width = 1200, height = 1200, scaling = 1.5)
+
+ragg::agg_png(filename = 'figures/sierra-ms-log-graph-01.png', width = 2000, height = 2000, scaling = 2.5)
 
 par(mar = c(0, 0, 0, 0))
 
 # set.seed(1010101)
-plot(g, vertex.label.family = 'sans', vertex.color = 'white', vertex.label.color = 'black', vertex.label.cex = 0.66, edge.arrow.size = 0.25, edge.color = 'royalblue', layout = layout_with_dh)
+plot(g, vertex.label.family = 'sans', vertex.color = 'white', vertex.label.color = 'black', edge.arrow.size = 0.25, edge.color = 'royalblue', layout = layout_with_dh)
 title('Sierra and Surrounding Meshtastic Network', line = -1.5, sub = 'excluding MQTT')
 
 dev.off()
 
 
-# TODO: do something fun with traceroute data
-cat(z$Data)
 
+
+
+ragg::agg_png(filename = 'figures/sierra-ms-log-graph-02.png', width = 1000, height = 1000, scaling = 1.66)
+
+par(mar = c(0, 0, 1, 0))
+
+# set.seed(1010101)
+plot(g.sub, vertex.label.family = 'sans', vertex.color = 'white', vertex.label.color = 'black', edge.arrow.size = 0.25, edge.color = 'royalblue', layout = layout_with_dh)
+title('Sierra and Surrounding Meshtastic Network\ndegree > 2', line = -1.5, sub = 'excluding MQTT')
+
+dev.off()
+
+
+
+
+# TODO: include community detection
+# plot(cluster_walktrap(g), g)
+# plot(cluster_fast_greedy(g), g)
+
+
+m <- membership(cluster_fast_greedy(g))
+
+cr <- colorRampPalette(hcl.colors(n = 20, palette = 'spectral'), space = 'Lab')
+cols <- cr(length(unique(m)))
+cols <- scales::alpha(cols, alpha = 0.5)
+
+
+
+
+ragg::agg_png(filename = 'figures/sierra-ms-log-graph-03.png', width = 2000, height = 2000, scaling = 2.5)
+
+par(mar = c(0, 0, 0, 0))
+
+plot(g, vertex.label.family = 'sans', vertex.color = cols[m], vertex.label.color = 'black', edge.arrow.size = 0.25, edge.color = 'royalblue', layout = layout_with_dh)
+title('Sierra and Surrounding Meshtastic Network', line = -1.5, sub = 'excluding MQTT')
+
+dev.off()
+
+
+
+
+# TODO: do something fun with traceroute data
+tr <- unique(z$Data)
+
+tr <- tr[order(sapply(tr, nchar))]
+
+cat(tr, sep = '\n')
 
 
 
